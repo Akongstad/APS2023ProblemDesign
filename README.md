59a01375d20c81bb7b6e4f0bd9db54ea688f4ea9
ches <ches@itu.dk>
2023-02-28 14:47:58 +0100

    java accepted

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # APS2023ProblemDesign
 Kattis problems designed for the course: Algorithmic Problem Solving | ITU 2023 
+Authors: Andreas Kongstad & Christian Emil Stender
 
 # Problems
 ## A-Counting
---
+++ acounting/submissions/accepted/Acounting.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Acounting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<Integer> a = new ArrayList<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            a.add(sc.nextInt());
+        }
+
+        int sum;
+
+        for (Integer integer : a) {
+            sum += integer;
+        }
+
+        System.out.println(sum);
+    }
+}
